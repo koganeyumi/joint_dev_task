b1fade79d5efad12a077de56e9f6552065586b14
koganeyumi <tamamisan0306@gmail.com>
2020-12-21 14:03:08 +0900

    rubyq20

--- main.rb
+++ main.rb
@@ -8,16 +8,55 @@ management = Management.new
 management.prompt_input
 management.output_answer
 
-class Item
-  # 以下を修正して下さい
-attr_reader :name
 
-  def initialize(name:)
-    @name = name
+class UserQ20
+  # 以下に回答を記載
+  attr_accessor :user_name, :user_age
+
+  def initialize(**params)
+    @user_name = params[:name]
+    @user_age = params[:age]
   end
 end
 
+class Zoo
+  # 以下に回答を記載
+   attr_accessor :name, :entry_fee
+  
+  def initialize(**params)
+    @zoo_name = params[:name]
+    @entry_fee = params[:entry_fee]
+  end
 
-  # ここは変更しないで下さい
-  book = Item.new(name: "ゼロ秒思考")
-  puts book.name
+  def info_entry_fee(user)
+  
+    case user.user_age
+    when 0..5
+      p "#{user.user_name}さんの入場料金は#{@entry_fee[:infant]}円です。"
+
+    when 6..12
+      p "#{user.user_name}さんの入場料金は#{@entry_fee[:children]}円です。"
+    when 13..64
+      p "#{user.user_name}さんの入場料金は#{@entry_fee[:adult]}円です。"
+
+    when 65..120
+      p "#{user.user_name}さんの入場料金は#{@entry_fee[:senior]}円です。"
+    end   
+  end
+end
+
+# def q20
+  # ここは変更しないで下さい（動物園・ユーザー情報は変更していただいてOKです）
+  zoo = Zoo.new(name: "旭山動物園", entry_fee: { infant: 0, children: 400, adult: 800, senior: 500 })
+
+  users = [
+    UserQ20.new(name: "たま", age: 3),
+    UserQ20.new(name: "ゆたぼん", age: 10),
+    UserQ20.new(name: "あじー", age: 32),
+    UserQ20.new(name: "ぎん", age: 108)
+  ]
+
+  users.each do |user|
+    zoo.info_entry_fee(user)
+  end
+# end
--- task.rb
+++ task.rb
@@ -229,14 +229,39 @@ end
 
 class UserQ20
   # 以下に回答を記載
+  attr_accessor :user_name, :user_age
 
+  def initialize(**params)
+    @user_name = params[:name]
+    @user_age = params[:age]
+  end
 end
 
 class Zoo
   # 以下に回答を記載
+   attr_accessor :name, :entry_fee
+  
+  def initialize(**params)
+    @zoo_name = params[:name]
+    @entry_fee = params[:entry_fee]
+  end
 
+  def info_entry_fee(user)
+  
+    case user.user_age
+    when 0..5
+      p "#{user.user_name}さんの入場料金は#{@entry_fee[:infant]}円です。"
+
+    when 6..12
+      p "#{user.user_name}さんの入場料金は#{@entry_fee[:children]}円です。"
+    when 13..64
+      p "#{user.user_name}さんの入場料金は#{@entry_fee[:adult]}円です。"
+
+    when 65..120
+      p "#{user.user_name}さんの入場料金は#{@entry_fee[:senior]}円です。"
+    end   
+  end
 end
-
 
 def q20
   # ここは変更しないで下さい（動物園・ユーザー情報は変更していただいてOKです）
